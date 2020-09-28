687f88b2b0ec9ec48a772ae025f1ce7d5848d59a
acquanero <pabloacquafresca@gmail.com>
2020-09-28 20:51:19 -0300

    add forgot password, register, y actionListener del boton login

--- app/src/main/java/com/acquanero/ezcard/MainActivity.java
+++ app/src/main/java/com/acquanero/ezcard/MainActivity.java
@@ -3,6 +3,9 @@ package com.acquanero.ezcard;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +15,15 @@ public class MainActivity extends AppCompatActivity {
         setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button loginButton = (Button) findViewById(R.id.button_login);
+
+        loginButton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
