e1804dacc0d8d0bb54b89f68b283724fe5fbd21b
Pablo <pabloacquafresca@gmail.com>
2020-10-04 15:27:14 -0300

    Extraccion del token y user id, y almacenamiento en el SharedPreferences

--- app/src/main/java/com/acquanero/ezcard/MainActivity.java
+++ app/src/main/java/com/acquanero/ezcard/MainActivity.java
@@ -1,7 +1,9 @@
 package com.acquanero.ezcard;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceManager;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -28,6 +30,10 @@ public class MainActivity extends AppCompatActivity {
 
     private UserIdToken useridtoken;
 
+    SharedPreferences dataDepot;
+
+    SharedPreferences.Editor dataDepotEditable;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -35,13 +41,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //recupero del layout los botones y los campos de texto
         Button loginButton = (Button) findViewById(R.id.button_login);
-
         mailUser = (TextView) findViewById(R.id.campo_usuario);
         password = (TextView) findViewById(R.id.campo_password);
 
+        //Traigo una instancia de retrofit para realizar los request
         myAPIService = ApiUtils.getAPIService();
 
+        //Creo una instancia de SahredPreference para almacenar informacion
+        //el archivo se encuentra en /data/data/[nombre del proyecto]/shared_prefs/archivo.xml
+        dataDepot = PreferenceManager.getDefaultSharedPreferences(this);
+
+        //Vuelvo editable mi SharedPreference
+        dataDepotEditable = dataDepot.edit();
+
+        //asocio el evento correspondiente al boton de login
         loginButton.setOnClickListener(new View.OnClickListener(){
 
             @Override
@@ -54,17 +69,26 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    //metodo a ejecutar al presionar el boton login
     public void logIn(String mail, String passw) {
         myAPIService.getUserInfo(mail, passw).enqueue(new Callback<UserIdToken>() {
             @Override
             public void onResponse(Call<UserIdToken> call, Response<UserIdToken> response) {
 
                 if(response.isSuccessful()) {
-                    int id = response.body().getUserId();
+
+                    //guardo el id y el token en una variable
+                    int idUsuario = response.body().getUserId();
                     String token = response.body().getToken();
 
+                    //almaceno el id y el token en el SharedPreference
+                    dataDepotEditable.putInt("user_id", idUsuario);
+                    dataDepotEditable.putString("token", token);
+                    dataDepotEditable.apply();
+
+                    //Chequeo que se hayan almacenado
                     System.out.println("----------------------------------");
-                    System.out.println("User id: " + id + " Token: " + token);
+                    System.out.println("User id: " + dataDepot.getInt("user_id", -1) + " Token: " + dataDepot.getString("token", "null"));
                     System.out.println("----------------------------------");
 
                     Log.i("RTA SUCCESS", "post submitted to API." + response.body().toString());
