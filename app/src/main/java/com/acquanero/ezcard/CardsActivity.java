8a979595fcf402fcf63b32a59a36e79d2c6d491e
Pablo <pabloacquafresca@gmail.com>
2020-10-19 18:34:45 -0300

    Correccion del display de las tarjetas utilizando LinearLayout dentro de GridLayout para mostrar imagen con texto abajo

--- app/src/main/java/com/acquanero/ezcard/CardsActivity.java
+++ app/src/main/java/com/acquanero/ezcard/CardsActivity.java
@@ -5,15 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.GridLayout;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.appcompat.widget.Toolbar;
-import androidx.core.content.ContextCompat;
 import androidx.core.content.res.ResourcesCompat;
 import androidx.preference.PreferenceManager;
 
@@ -25,7 +26,6 @@ public class CardsActivity extends AppCompatActivity {
 
     SharedPreferences dataDepot;
     private Toolbar toolbar;
-    private GridLayout gridCards;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,28 +35,41 @@ public class CardsActivity extends AppCompatActivity {
         toolbar = findViewById(R.id.toolbarCards);
         setSupportActionBar(toolbar);
 
-        gridCards = (GridLayout) findViewById(R.id.gridCards);
-
         //Creo una instancia de SahredPreference para almacenar informacion
         //el archivo se encuentra en /data/data/[nombre del proyecto]/shared_prefs/archivo.xml
         dataDepot = PreferenceManager.getDefaultSharedPreferences(this);
 
         String userJson = dataDepot.getString("usuario", "null");
-
         Gson gson = new Gson();
+        UserData userData = gson.fromJson(userJson, UserData.class);
 
-        UserData u = gson.fromJson(userJson, UserData.class);
 
-        for (Card c : u.getCards()){
+        //Guardo el GridLayout del CardsActivity en una variable
+        GridLayout gridCards = (GridLayout) findViewById(R.id.gridCards);
 
-            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(GridLayout.LayoutParams.WRAP_CONTENT, GridLayout.LayoutParams.WRAP_CONTENT);
-            params.setMargins(25, 25, 0, 0);
-            Button mb = new Button(getApplicationContext());
-            mb.setText(c.getName());
-            mb.setLayoutParams(params);
-            mb.setBackground(ResourcesCompat.getDrawable(getResources(), R.drawable.ic_launcher_background, null));
+        //Con un for each recorro la lista de tarjetas y genero el imageButton con un label por cada tarjeta
+        //y los inserto en un linearLayout vertical
+        //y a su vez este ultimo, lo inserto en cada celda del GridLayout
+        for (Card card : userData.getCards()){
 
-            gridCards.addView(mb);
+            LinearLayout linearLayoutInsideGrid = new LinearLayout(getApplicationContext());
+            LinearLayout.LayoutParams paramsLinear = new LinearLayout.LayoutParams(GridLayout.LayoutParams.WRAP_CONTENT, GridLayout.LayoutParams.WRAP_CONTENT);
+            linearLayoutInsideGrid.setOrientation(LinearLayout.VERTICAL);
+            linearLayoutInsideGrid.setGravity(Gravity.CENTER);
+            paramsLinear.setMargins(0, 0, 55, 20);
+
+            Button botonImage = new Button(getApplicationContext());
+            //botonImage.setText(card.getName());
+            TextView txt = new TextView(getApplicationContext());
+            txt.setText(card.getName());
+            txt.setGravity(Gravity.CENTER);
+            botonImage.setBackground(ResourcesCompat.getDrawable(getResources(), R.drawable.ic_launcher_background, null));
+
+            linearLayoutInsideGrid.addView(botonImage, paramsLinear);
+            linearLayoutInsideGrid.addView(txt,paramsLinear);
+
+            gridCards.addView(linearLayoutInsideGrid);
+
 
         }
     }
