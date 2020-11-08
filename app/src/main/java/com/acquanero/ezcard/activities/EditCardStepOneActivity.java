1b233f15e5b9a02be4a4257cf79cbba105027dd2
Pablo <pabloacquafresca@gmail.com>
2020-11-07 22:21:18 -0300

    Se renombra el archivo activity_edit_card.xml a activity_edit_card_step_one.xml

--- app/src/main/java/com/acquanero/ezcard/activities/EditCardStepOneActivity.java
+++ app/src/main/java/com/acquanero/ezcard/activities/EditCardStepOneActivity.java
@@ -29,7 +29,7 @@ public class EditCardStepOneActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_edit_card);
+        setContentView(R.layout.activity_edit_card_step_one);
 
         Bundle datos = getIntent().getExtras();
 
